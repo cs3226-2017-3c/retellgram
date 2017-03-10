84a9f6bca78f5cafd71e8df5ab6cdb92ba3372f6
HangZhao <davidzhao800@gmail.com>
2017-03-10 17:52:48 +0800

    migration and seeder file

---
+++ backend/database/migrations/2017_03_10_080608_Create_Image_Table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('filepath');
+            $table->string('md5');
+            $table->integer('likes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
---
+++ backend/database/seeds/CaptionSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CaptionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	$faker = Faker\Factory::create();
+    	$limit = 50;
+
+    	$test_images_path="public/test_images/";
+
+    	$test_images = ["test-01.png",
+    					"test-02.png",
+    					"test-03.png",
+    					"test-04.png",
+    					"test-05.png",
+    					"test-06.png",
+    					"test-07.png",
+    					"test-08.png",
+    					"test-09.png"];
+
+        foreach ($test_images as $image) {
+            DB::table('images')->insert([ 
+                'filepath' => $test_images_path . $image,
+                'md5' => md5_file ( storage_path() . "/app/" . $test_images_path . $image),
+                'likes' => 0
+            ]);
+        }
+
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('captions')->insert([ 
+                'image_id' => $faker->numberBetween(1, sizeof( $test_images)),
+                'content' => $faker->text($maxNbChars = 50),
+                'likes' => 0
+            ]);
+        }
+        
+    }
+}
