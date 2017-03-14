5d9c6ffd8e25f0c7e4830ceed8ab4643b76ba625
XiaoKangwei <cocolonel.x@gmail.com>
2017-03-14 22:04:17 +0800

    - implement create a caption

--- backend/app/Http/Controllers/CaptionController.php
+++ backend/app/Http/Controllers/CaptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use App\Caption;
@@ -9,6 +10,21 @@ use App\Image;
 
 class CaptionController extends Controller
 {
+	public function createCaption(Request $request) {
+		$body = json_decode($request->getContent(), true);
+		$this->validateCreateCaption($body);
+		$this->validateImageExist($body["image_id"]);
+
+		$new_caption = new Caption;
+
+		$new_caption->image_id = $body["image_id"];
+		$new_caption->content = $body["content"];
+		$new_caption->likes = 0;
+		$new_caption->save();
+
+		return response($new_caption->id);
+	}
+
     public function getCaptions() {
         
         $captions = Caption::All();
@@ -29,7 +45,7 @@ class CaptionController extends Controller
     	if (sizeof($query) == 0) {
     		return $this->getCaptions();
     	}
-    	$this->validateQuery($query);
+    	$this->validateGetQuery($query);
 
     	//step 2. retrieve data according to query
     	$image_id = $query["image_id"];
@@ -47,11 +63,27 @@ class CaptionController extends Controller
     	return response(204);
     }
 
-    private function validateQuery($query) {
+    private function validateGetQuery($query) {
     	//naive validate: the url query must conatin image_id parameter
     	if (!array_key_exists("image_id", $query)) {
     		abort(404, 'Page Not Found');
     	}	
+    }
+
+    private function validateCreateCaption($body) {
+    	$validator = Validator::make($body, [
+            'image_id' => array('required','integer'),
+            'content' => array('required','min:1','max:50','regex:/^[A-Za-z0-9,._ ]+$/'),
+        ]);
+
+    	if ($validator->fails()) {
+            abort(400, 'Bad Request');
+        }
+    }
+
+    private function validateImageExist($image_id) {
+    	$images = Image::find($image_id);
+    	$this->checkResult($images);
     }
 
     private function checkResult($array) {
