41bede42b5c406d21e1b2dc1715369bc790182f0
HangZhao <davidzhao800@gmail.com>
2017-03-16 16:09:14 +0800

    template setup

---
+++ backend/resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-12">
+        <p class="text-right">© 2017 M. Ashmawi, C. G. Lim, Z. Lin, K. Xiao, S. F. Zarrinkoub & H. Zhao. <a href="https://github.com/cs3226-2017-3c" target="_blank"><img width="12" alt="GitHub" src="/img/GitHub-Mark-32px.png"></a></p>
+      </div>
+
+    </div>
+  </div>
+  
+</footer>
---
+++ backend/resources/views/template.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>@yield('title') - Retellgram</title>
+
+  <!-- Bootstrap -->    
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  
+  @yield('header')
+  
+  <link href="/css/navbar.css" rel="stylesheet">
+
+</head>
+<body>
+
+  <div class="container">
+    @include('flash::message')
+  </div>
+  @include('_navigation')
+   
+  @yield('main')
+   
+  @include('_footer')
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script>
+  $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+  </script>
+    
+
+
+  @yield('footer')
+</body>
+</html>
