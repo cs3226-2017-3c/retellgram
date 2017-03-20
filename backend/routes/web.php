ef819d5d295bad21b57b9c4dff95368234d21c0c
linzr <noctislin@gmail.com>
2017-03-20 12:20:50 +0800

    adjust route.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,30 +14,36 @@
 /*
 	Routes
 */
+
+
 Route::get('upload', 'UploadController@viewUpload');
-Route::post('/uploadCrop', 'UploadController@cropUpload');
+Route::post('uploadCrop', 'UploadController@cropUpload');
 
 Route::get('create', 'CreateController@viewCreate');
 Route::post('create', 'CreateController@storeCreate');
-Route::get('/selectimage', 'CreateController@viewSelectImage');
-Route::post('/selectimage', 'CreateController@submitSelectImage');
+Route::get('selectimage', 'CreateController@viewSelectImage');
+Route::post('selectimage', 'CreateController@submitSelectImage');
+
+Route::get('/', 'HomeController@home');
 
 //Route::post('/chooseImage', 'CreateController@chooseImage');
+
 /*
 	APIs
 */
-Route::get('/', 'TestViewController@getTestView');
 
-Route::get('/getCaptions', 'CaptionController@getCaptions');
-Route::get('/getImages', 'ImageController@getImages');
+Route::group(['prefix' => 'api'], function () {
+  Route::get('/getCaptions', 'CaptionController@getCaptions');
+  Route::get('/getImages', 'ImageController@getImages');
 
-Route::post('/image', 'ImageController@createImage');
-Route::get('/image/{id}', 'ImageController@getImage');
-Route::get('/image', 'ImageController@getImages');
-Route::delete('/image/{id}', 'ImageController@deleteImage');
+  Route::post('/image', 'ImageController@createImage');
+  Route::get('/image/{id}', 'ImageController@getImage');
+  Route::get('/image', 'ImageController@getImages');
+  Route::delete('/image/{id}', 'ImageController@deleteImage');
 
-Route::post('/caption', 'CaptionController@createCaption');
-Route::get('/caption/{id}', 'CaptionController@getCaption');
-Route::get('/caption', 'CaptionController@getCaptionsWithQuery');
-Route::put('/caption/{id}', 'CaptionController@likeCaption');
-Route::delete('/caption/{id}', 'CaptionController@deleteCaption');
+  Route::post('/caption', 'CaptionController@createCaption');
+  Route::get('/caption/{id}', 'CaptionController@getCaption');
+  Route::get('/caption', 'CaptionController@getCaptionsWithQuery');
+  Route::put('/caption/{id}', 'CaptionController@likeCaption');
+  Route::delete('/caption/{id}', 'CaptionController@deleteCaption');
+});
