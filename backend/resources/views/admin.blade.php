9c2e942d2535ecbba58865c0c45f47bcb04d9c04
Sahand Zarrinkoub <sahandz@kth.se>
2017-03-26 16:35:19 +0800

    admin page

--- backend/resources/views/admin.blade.php
+++ backend/resources/views/admin.blade.php
@@ -7,8 +7,15 @@ Admin Page
 @section('main')
 
 @foreach ($unverified as $caption)
-<p value='{{$caption->id}}'>{{$caption->content}}</p>							
+<p value='{{$caption->id}}'>{{$caption->content}}</p>
+<form action="caption/{{$caption->id}}/delete" method="post">
+<button type="submit">Delete caption</button>
+</form>	
+<form action="caption/{{$caption->id}}" method="post">
+<button type="submit">Accept caption</button>
+</form>			
 @endforeach
+
 
 @endsection
 @section('footer')
