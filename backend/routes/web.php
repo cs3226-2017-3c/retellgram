6ff81c9de1ce2a26037aaba821669271e69a47d7
HangZhao <davidzhao800@gmail.com>
2017-03-30 09:50:24 +0800

    clear unused controller and view

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,22 +15,13 @@
 	Routes
 */
 
-
-Route::get('upload', 'UploadController@viewUpload');
-Route::post('upload', 'UploadController@storeUpload');
-
 Route::get('new_upload', 'UploadControllerNew@viewUpload');
 Route::post('new_upload', 'UploadControllerNew@storeUpload');
 
-//Route::post('uploadCrop', 'UploadController@cropUpload');
 
 Route::get('new_create', 'CreateControllerNew@viewCreate');
 Route::post('new_create', 'CreateControllerNew@storeCreate');
 
-Route::get('create', 'CreateController@viewCreate');
-Route::post('create', 'CreateController@storeCreate');
-Route::get('selectimage', 'CreateController@viewSelectImage');
-Route::post('selectimage', 'CreateController@submitSelectImage');
 
 Route::get('detail/{id}', 'DetailController@getView');
 Route::get('image/{id}', 'DetailController@getImage');
@@ -44,7 +35,6 @@ Route::post('caption/{id}/approve', 'DetailController@approveCaption');
 
 Route::get('/', 'HomeController@home');
 
-//Route::post('/chooseImage', 'CreateController@chooseImage');
 
 Route::get('admin', 'AdminController@admin');
 Auth::routes();
