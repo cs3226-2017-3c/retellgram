7e6b443c8ed253550c8a20082e3ced682ae9313c
Sahand Zarrinkoub <sahandz@kth.se>
2017-03-30 13:48:04 +0800

    changed login path to 123
    To change login path to a URL of your choice, edit the routes/web.php
    file.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -42,3 +42,18 @@ Route::get('admin', 'AdminController@admin');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+// Authentication Routes...
+Route::get('123', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('123', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
