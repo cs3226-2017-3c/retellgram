a84ac34763c6040459657a7bb613a6dbb25cc25d
HangZhao <davidzhao800@gmail.com>
2017-03-31 18:17:24 +0800

    back to top button

--- backend/resources/views/new_template.blade.php
+++ backend/resources/views/new_template.blade.php
@@ -37,6 +37,8 @@
    
   @yield('main')
 
+  <button class="cd-top" title="Go to top"></button>
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="/js/docs.min.js"></script>
   <script src="/js/google-analytics.js"></script>
@@ -49,6 +51,37 @@
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
   <script>
   $('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+  </script>
+
+  <script>
+  $(document).ready(function($){
+  // browser window scroll (in pixels) after which the "back to top" link is shown
+  var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700,
+    //grab the "back to top" link
+    $back_to_top = $('.cd-top');
+
+  //hide or show the "back to top" link
+  $(window).scroll(function(){
+    ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    if( $(this).scrollTop() > offset_opacity ) { 
+      $back_to_top.addClass('cd-fade-out');
+    }
+  });
+
+  //smooth scroll to top
+  $back_to_top.on('click', function(event){
+    event.preventDefault();
+    $('body,html').animate({
+      scrollTop: 0 ,
+      }, scroll_top_duration
+    );
+  });
+
+});
   </script>
 
   @yield('footer')
