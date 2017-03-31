5e2156199cfbfacf67b0168044629d0179bb13b7
XiaoKangwei <cocolonel.x@gmail.com>
2017-03-31 16:52:07 +0800

    change the scheduler to run every hour

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends ConsoleKernel
                 $characterNewLike->new_like = $aRecord->new_like;
                 $characterNewLike->save();
             }
-        })->everyMinute();
+        })->hourly();
     }
 
     /**
