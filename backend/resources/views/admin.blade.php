667cb1134e0a215c7c50d682aeab5baa94c1c634
HangZhao <davidzhao800@gmail.com>
2017-04-01 14:19:12 +0800

    admin page completed and clear controllers

--- backend/resources/views/admin.blade.php
+++ backend/resources/views/admin.blade.php
@@ -1,22 +1,28 @@
-@extends('template')
+@extends('new_template')
 @section('title')
-Admin Page
+Admin
 @endsection
 @section('header')
 @endsection
 @section('main')
+<div id="wrapper">
 
-@foreach ($unverified as $caption)
-<p value='{{$caption->id}}'>{{$caption->content}}</p>
-<form action="caption/{{$caption->id}}/delete" method="post">
-<button type="submit">Delete caption</button>
-</form>	
-<form action="caption/{{$caption->id}}/approve" method="post">
-<button type="submit">Accept caption</button>
-</form>			
-@endforeach
+<div class="row">
+    <div class="container">
+		@foreach ($images as $image)
+	    <div class="col-md-4">
+	      	<img src="storage/images/{{ $image->file_path }}" class="img-rounded">
+			<form action="image/{{$image->id}}/delete" method="post">
+				{{ csrf_field() }}
+				<button type="submit">Delete Image</button>
+			</form>	
+		</div>
+		@endforeach
+	</div>
+</div>
 
 
+</div>
 @endsection
 @section('footer')
 @endsection
