8dc24d8aca7053d970da5af34f8b4d3b5c951f97
HangZhao <davidzhao800@gmail.com>
2017-04-10 14:47:51 +0800

    change view of admin

--- backend/resources/views/admin.blade.php
+++ backend/resources/views/admin.blade.php
@@ -13,7 +13,7 @@ Admin
           <div class="panel-body">
 			@foreach ($images as $image)
 		    <div class="col-md-4">
-		    	<div style="margin-down:20px;position:relative;margin-bottom:20px;">
+		    	<div style="position:relative;margin-bottom:50px;">
 			      	<a href="/thisrouteshouldhidefromuseradmin/{{$image->id}}">
 			      		<img style="width:200px;height:200px;" src="/storage/images/{{ $image->file_path }}" class="img-rounded">
 			      	</a>
@@ -21,7 +21,6 @@ Admin
 						{{ csrf_field() }}
 						<button type="submit" style="position:absolute;">Delete Image</button>
 					</form>
-					<br>
 					<br>
 					@if(Request::path()=="thisrouteshouldhidefromuserreport")
 					<form action="image/{{$image->id}}/resetReports" method="post">
